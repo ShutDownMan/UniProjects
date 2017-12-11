0fcac3ea6d46c2e0c922553c367340b245b0e21e
ShutDownMan <jedson_gabriel@hotmail.com>
2017-12-11 21:24:21 -0200

    Add some comments
    Commenting lines and debugs

--- C/Projects/Restaurante/main.c
+++ C/Projects/Restaurante/main.c
@@ -50,23 +50,29 @@ Item* findItem(char itemName[], Item** items, int quantItems);
 int debugRelat(char fileName[], OrderData** orders, int quantOrders);
 
 int main(int argc, char const *argv[]) {
+	// orders store all orders of the day
 	OrderData** orders = NULL;
+	// waiters store all know waiters
 	Waiter** waiters = NULL;
+	// items store all known items
 	Item** items = NULL;
 	int quantOrders, quantWaiters, quantItems;
 
+	// alloc memory for the arrays
 	orders = malloc(sizeof(OrderData*) * MAXPTR);
 	waiters = malloc(sizeof(Waiter*) * MAXPTR);
 	items = malloc(sizeof(Item*) * MAXPTR);
 
+	// If we load we good
 	if(loadData("consumo.txt", orders, &quantOrders, waiters, &quantWaiters, items, &quantItems)) return 1;
 
+	// Try'na not occupy unused space
 	orders = realloc(orders, sizeof(OrderData*) * (quantOrders));
 	waiters = realloc(waiters, sizeof(Waiter*) * (quantWaiters));
 	items = realloc(items, sizeof(Item*) * (quantItems));
 
+	// Let see if things loaded correctly
 	debugRelat("debugrelat.txt", orders, quantOrders);
-
 
 	return 0;
 }
@@ -78,27 +84,38 @@ int loadData(char fileName[], OrderData** orders, int* quantOrders, Waiter** wai
 	struct tm date;
 	int i;
 
+	// Set it all to ZERO/NADA/NOTHING/NULL
 	*quantOrders = *quantWaiters = *quantItems = 0;
+	// open file with data
 	if(!(f = fopen(fileName, "r"))) return 1;
 
+	// Load day of data
 	loadDate(f, &date);
 
+	// read all orders
 	for (i = 0; !feof(f); ++i) {
+		// alloc memory for data
 		orders[i] = malloc(sizeof(OrderData));
 
+		// get table waiter
 		loadWaiter(f, orders[i], waiters, quantWaiters);
 
+		// get table ID
 		fscanf(f, "%*s %d%*c", &orders[i]->tableID);
 
-		printf("table => %d\n", orders[i]->tableID);
+		// printf("table => %d\n", orders[i]->tableID);
 
+		// Get hour of meal(?)
 		fscanf(f, "%d%*c%d%*c", &orders[i]->startTimeStamp.tm_hour, &orders[i]->startTimeStamp.tm_min);
 		fscanf(f, "%d%*c%d%*c", &orders[i]->endTimeStamp.tm_hour, &orders[i]->endTimeStamp.tm_min);
 
+		// get quant of people at table
 		fscanf(f, "%d%*c", &orders[i]->quantPeople);
 
+		// get consumed items
 		loadConsumed(f, orders[i], items, quantItems);
 
+		// Increase order count
 		(*quantOrders)++;
 	}
 
@@ -111,84 +128,109 @@ void loadConsumed(FILE* f, OrderData* order, Item** items, int* quantItems) {
 	int currItemQuant;
 	float currItemPrice;
 
+	// alloc memory for order array
 	order->consumed = malloc(sizeof(Order*) * MAXPTR);
 
+	// initialize cosumed items to ZERO
 	order->quantConsumed = 0;
 
+	// scan current line
 	fscanf(f, "%s%*c", currItemName);
+	// if not end keep reading
 	for (i = 0; strcmp(currItemName, "<fim>"); ++i) {
+		// got here, then read quant and price
 		fscanf(f, "%d %f%*c", &currItemQuant, &currItemPrice);
 
+		// alloc memory for that item
 		order->consumed[i] = malloc(sizeof(Order));
 
-		printf("[%d] => %s\n", i, currItemName);
+		// printf("[%d] => %s\n", i, currItemName);
 
+		// store item quantity and increase item array counter
 		order->consumed[i]->quant = currItemQuant;
 		order->quantConsumed++;
 
+		// if current item not found in list
 		if(!(order->consumed[i]->orderedItem = findItem(currItemName, items, *quantItems))) {
 
+			// create a new one
 			order->consumed[i]->orderedItem = malloc(sizeof(Item));
 
+			// store id, name and price
 			order->consumed[i]->orderedItem->id = *quantItems;
 			order->consumed[i]->orderedItem->name = strcpy(malloc(sizeof(char) * strlen(currItemName)), currItemName);
 			order->consumed[i]->orderedItem->price = currItemPrice;
 
+			// add pointer to array and update list counter
 			items[*quantItems] = order->consumed[i]->orderedItem;
 			(*quantItems)++;
 		}
 
+		// scan for a new item
 		fscanf(f, "%s%*c", currItemName);
 	}
+	// Gotta use less space o/
 	realloc(order->consumed, sizeof(Order*) * (order->quantConsumed));
-	printf("------\n");
+	// printf("------\n");
 }
 
 Item* findItem(char itemName[], Item** items, int quantItems) {
 	int i;
 
+	// for each item in array
 	for (i = 0; i < quantItems; ++i) {
+		// if found item, return it
 		if(!strcmp(items[i]->name, itemName)) return items[i];
 	}
 
+	// if not, return 0
 	return 0;
 }
 
 void loadWaiter(FILE* f, OrderData* order, Waiter** waiters, int* quantWaiters) {
 	char waiterName[MAXSTR];
 
+	// scan waiter name
 	fscanf(f, "%[^\n]%*c", waiterName);
 
-	printf("name = %s / quant = %d\n", waiterName, *quantWaiters);
+	// printf("name = %s / quant = %d\n", waiterName, *quantWaiters);
 
+	// if waiter not found in array
 	if(order->mainWaiter = findWaiter(waiterName, waiters, *quantWaiters)) return;
 
+	// create a new one
 	order->mainWaiter = malloc(sizeof(Waiter));
 
+	// store it's id and name
 	order->mainWaiter->id = *quantWaiters;
 	order->mainWaiter->name = strcpy(malloc(sizeof(char) * strlen(waiterName)), waiterName);
 
+	// add pointer to array and update array counter
 	waiters[*quantWaiters] = order->mainWaiter;
 	(*quantWaiters)++;
 
-	printf("waiters = %d\n", *quantWaiters);
+	// printf("waiters = %d\n", *quantWaiters);
 }
 
 Waiter* findWaiter(char waiterName[], Waiter** waiters, int quantWaiters) {
 	int i;
 
+	// for each waiter in array
 	for (i = 0; i < quantWaiters; ++i) {
+		// if waiter found, return it
 		if(!strcmp(waiters[i]->name, waiterName)) return waiters[i];
 	}
 
+	// if not, return 0
 	return 0;
 }
 
 
 void loadDate(FILE* f, struct tm* date) {
+	// read day/month/year as it should be
 	fscanf(f, "%d/%d/%d%*c", &(date->tm_mday), &(date->tm_mon), &(date->tm_year));
 
-	fprintf(stderr, "%d/%d/%d\n", date->tm_mday, date->tm_mon, date->tm_year);
+	// fprintf(stderr, "%d/%d/%d\n", date->tm_mday, date->tm_mon, date->tm_year);
 }
 
 // PRINT DATA
