ea3d12011bf9930f3ea4ce0b5b8f1613d49bad88
ShutDownMan <jedson_gabriel@hotmail.com>
2017-12-17 17:44:33 -0200

    Commenting Senhas
    and also translating some things to my convention

--- C/Pessoas/Horst/Senha/main.c
+++ C/Pessoas/Horst/Senha/main.c
@@ -4,73 +4,94 @@
 #include <string.h>
 #define MAX 100
 
-typedef struct codigo
-{
+typedef struct codigo {
 	char *senha;
 	int indice;
-}_codigo;
+} codigo;
 
-int load (char *fn, _codigo *cripto[], int* num_senhas);
-int analisaSenha( _codigo *cripto[], int num_senhas);
+int load (char *fn, codigo *cripto[], int* num_senhas);
+int analisaSenha( codigo *cripto[], int num_senhas);
 int senha (char *str);
-int relatorio (char * fn, _codigo *cripto[], int num_senhas, int tipo);
-int relat(char *fn, _codigo *cripto[], int num_senhas);
-void libera(_codigo *cripto[], int num_senhas);
-int relatDecres(char *fn, _codigo *cripto[], int num_senhas);
-void first(_codigo *cripto[], int num_senhas);
-int getHighest(_codigo *cripto[], int num_senhas);
-void swap(_codigo** a, _codigo** b);
+
+int relatorio (char * fn, codigo *cripto[], int num_senhas, int tipo);
+int relat(char *fn, codigo *cripto[], int num_senhas);
+
+int relatDecres(char *fn, codigo *cripto[], int num_senhas);
+void first(codigo *cripto[], int num_senhas);
+int getHighest(codigo *cripto[], int num_senhas);
+void swap(codigo** a, codigo** b);
+
+void libera(codigo *cripto[], int num_senhas);
 
 int main(int argc, char const *argv[]) {
-	_codigo **cripto; // ponteiro que aponta pro vetor de ponteiro
+	// vetor de ponteiro de codigo
+	codigo **cripto;
+	// tamanho do vetor
 	int num_senhas;
 
-	cripto = malloc(MAX * sizeof(_codigo*)); // aloca espaço para 100 ponteiro de struct _codigo
+	// aloca espaço para 100 ponteiro de struct codigo
+	cripto = malloc(MAX * sizeof(codigo*));
 
+	// carrega para a memória as senhas do arquivo "senhas.txt"
 	if(!load("senhas.txt", cripto, &num_senhas)) {
 
-		cripto = realloc(cripto, num_senhas*sizeof(_codigo*)); // realocando com o tamanho exato do vetor
+		// realocando com o tamanho exato do vetor
+		cripto = realloc(cripto, num_senhas*sizeof(codigo*));
 
+		// gera os indices das senhas
 		analisaSenha(cripto, num_senhas);
 
+		// gera relatório com indices
 		relatorio("indice.txt", cripto, num_senhas, 1);
+		// gera relatório ordenado (decrescente)
 		relatorio("indice2.txt", cripto, num_senhas, 2);
 
+		// libera toda memória dinamicamente alocada
 		libera(cripto, num_senhas);
 	}
+
 	return 0;
 }
 
-int load (char *fn, _codigo *cripto[], int* num_senhas) {
+int load (char *fn, codigo *cripto[], int* num_senhas) {
 	FILE* f;
 	int i;
 
+	// abre arquivo
 	f = fopen(fn, "r"); if(!f) return 1;
 
+	// enquanto não final de arquivo
 	for(i = 0; !feof(f); i ++) {
 
-		cripto[i] = malloc(sizeof(_codigo));
+		// aloca memoria para struct codigo
+		cripto[i] = malloc(sizeof(codigo));
+		// aloca memoria para string senha
 		cripto[i]->senha = malloc(sizeof(char)*MAX);
 
+		// lê linha do arquivo e armazena em senha
 		fscanf(f, "%[^\n]%*c", cripto[i]->senha);
 
+		// realoca memoria para tamanho de senha
 		cripto[i]->senha = realloc(cripto[i]->senha, sizeof(char)*strlen(cripto[i]->senha));
 	}
 
+	// num de senhas lidas = i
 	*num_senhas = i;
+	// fecha arquivo
 	fclose(f);
 
 	return 0;
 }
 
-int analisaSenha(_codigo *cripto[], int num_senhas) {
+int analisaSenha(codigo *cripto[], int num_senhas) {
 	int i;
 
+	// para cada senha i
 	for(i = 0; i < num_senhas; i++) {
-
+		// calcule indice da senha
 		cripto[i]->indice = senha(cripto[i]->senha);
-
 	}
+
 	return 0;
 }
 
@@ -84,21 +105,30 @@ int senha (char *str) {
 
 	if(strlen(str) >= 8) indice += 20;
 
+	// para cada char "i" de senha
 	for(i = 0; str[i]; i++) {
+		// str[i] é minusculo?
 		if(islower(str[i])) {
 			l_minuscula++;
-		} else
+		} else {
+			// é maiúsculo?
 			if(isupper(str[i])) {
 				l_maiuscula++;
-			} else
+			} else {
+				// é número
 				if(isdigit(str[i])) {
 					dig_num++;
-				} else
+				} else {
+					// não é alfa numérico?
 					if(!isalnum(str[i])) {
 						carac_esp++;
+					}
 				}
+			}
+		}
 	}
 
+	// add nos indices
 	if(l_minuscula) indice += 20;
 	if(l_maiuscula) indice += 20;
 	if(dig_num) indice += 20;
@@ -107,8 +137,9 @@ int senha (char *str) {
 	return indice;
 }
 
-int relatorio (char* fn, _codigo *cripto[], int num_senhas, int tipo) {
+int relatorio (char* fn, codigo *cripto[], int num_senhas, int tipo) {
 
+	// para cada tipo de relatório
 	switch(tipo) {
 		case 1: relat(fn, cripto, num_senhas);
 		case 2: relatDecres(fn, cripto, num_senhas);
@@ -117,57 +148,65 @@ int relatorio (char* fn, _codigo *cripto[], int num_senhas, int tipo) {
 	return 0;
 }
 
-int relat(char *fn, _codigo *cripto[], int num_senhas) {
+int relat(char *fn, codigo *cripto[], int num_senhas) {
 	FILE* frelat;
 	int i;
 
+	// abr arquivo
 	frelat = fopen(fn, "w"); if(!frelat) return 1;
 
+	// para cada senha
 	for(i = 0; i < num_senhas; i++) {
-
 		fprintf(frelat, "SENHA\t\t\t");
 		fprintf(frelat, "INDICE\n");
 
-		fprintf(frelat, "%s\t\t", cripto[i]->senha);
+		fprintf(frelat, "%-24s", cripto[i]->senha);
 		fprintf(frelat, "%d\n", cripto[i]->indice);
 	}
 }
 
-int relatDecres(char *fn, _codigo *cripto[], int num_senhas) {
+int relatDecres(char *fn, codigo *cripto[], int num_senhas) {
 	int i;
 	FILE* frelat;
 
+	// abre arquivo
 	frelat = fopen(fn, "w"); if(!frelat) return 1;
 
+	// para cada senha
 	for (i = 0; i < num_senhas; ++i) {
+		// troque o maior pela atual
 		first(&cripto[i], num_senhas-i);
 	}
 
+	// para cada senha
 	for(i = 0; i < num_senhas; i++) {
-
 		fprintf(frelat, "SENHA\t\t\t");
 		fprintf(frelat, "INDICE\n");
 
-		fprintf(frelat, "%s\t\t", cripto[i]->senha);
+		fprintf(frelat, "%-24s", cripto[i]->senha);
 		fprintf(frelat, "%d\n", cripto[i]->indice);
 	}
 	return 0;
 }
 
-void first(_codigo *cripto[], int num_senhas) {
+void first(codigo *cripto[], int num_senhas) {
 	int ind;
 
+	// pega a senha com maior indice
 	ind = getHighest(cripto, num_senhas);
 
+	// se for maior troca de lugar
 	if(cripto[ind]->indice > cripto[0]->indice) {
 		swap(&cripto[0], &cripto[ind]);
 	}
 }
 
-int getHighest(_codigo *cripto[], int num_senhas) {
+int getHighest(codigo *cripto[], int num_senhas) {
 	int i, ind = 0;
 
+	// para cada senha
 	for (i = 1; i < num_senhas; ++i) {
+		// acha o maior indice
 		if(cripto[i]->indice > cripto[ind]->indice) {
 			ind = i;
 		}
@@ -175,13 +214,13 @@ int getHighest(_codigo *cripto[], int num_senhas) {
 	return ind;
 }
 
-void swap(_codigo** a, _codigo** b) {
-	_codigo* tmp = *a;
+void swap(codigo** a, codigo** b) {
+	codigo* tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-void libera (_codigo *cripto[], int num_senhas) {
+void libera (codigo *cripto[], int num_senhas) {
 	int i;
 
 	for( i = 0; i < num_senhas; i++) {
