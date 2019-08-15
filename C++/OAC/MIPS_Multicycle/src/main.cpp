07c14d1ce751f4fd18ee327309018eaf564a8203
ShutDownMan <jedson_gabriel@hotmail.com>
2019-08-15 04:59:42 -0300

    Almost there

--- C++/OAC/MIPS_Multicycle/src/main.cpp
+++ C++/OAC/MIPS_Multicycle/src/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "Machine/Machine.h"
 
-int main() {
+int main(int argc, char const *argv[]) {
     auto *machine = new Machine();
+    unsigned char verbosity = 0;
 
     Machine::debugInfo("Initializing Machine", 1);
-    machine->initialize("teste3.txt", 3);
 
-    Machine::debugInfo("Clocking Machine", 1);
+    if(argc >= 3) {
+        verbosity = strtol(argv[2], nullptr, 10);
+    }
+
+    if(argc < 2) {
+        printf("Usage: \n"
+               "program assembly.txt [verbosity_level]\n");
+        return 1;
+    }
+
+    machine->initialize(argv[1], verbosity);
 
     int i = 0;
 
     do {
+        Machine::debugInfo("Clocking Machine", 1);
         machine->clock();
-    } while (i++ < 64 && getchar() != 'q');
+        printf("Type any key for the next clock\n"
+               "Press 'q' to quit");
+    } while (i++ < 128 && getchar_unlocked() != 'q');
+
     return 0;
 }
